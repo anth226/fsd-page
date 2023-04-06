1554e672a0b0dd3e2e321dd5893453aacf758212
wasiq abdullah <chwasiq0569>
2023-04-07 04:12:52 +0500

    Implemented landing screen

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "./landingpage.module.scss"
 import Logo from "../../assets/icons/Logo.png"
 import CrossIcon from "../../assets/icons/crossicon.png"
+import ActionBtnIcon from '../../assets/icons/actiobtnico.png'
 
 const LandingPage = () => {
     return (<div className={styles.landingPageContainer}>
@@ -19,18 +20,26 @@ const LandingPage = () => {
                 </div>
             </div>
         </div>
-        <div className={styles.sidebarContainer}>
-
-            <div className={styles.verticalTextContainer}>
-                <p>// THE MODERN COMPANY</p>
-            </div>
-            <div className={styles.codeContainer}>
-                <p>001</p>
-            </div>
-        </div>
         <div className={styles.contentContainer}>
-
+            <div className={styles.sidebarContainer}>
+                <div className={styles.verticalTextContainer}>
+                    <p>// THE MODERN COMPANY</p>
+                </div>
+                <div className={styles.codeContainer}>
+                    <p>001</p>
+                </div>
+            </div>
+            <div className={styles.mainContent}>
+                <button className={styles.actioBtn}>
+                    <img src={ActionBtnIcon} />
+                    <p>REVEAL NOW</p>
+                </button>
+                <div className={styles.footer}>
+                    <p> © 2023 TPW - ALL RIGHTS RESERVED.</p>
+                </div>
+            </div>
         </div>
+
     </div>);
 }
 
