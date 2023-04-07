57bc8ecea6cf7427ef8f05ce3ea783c6b8c4d5b0
wasiq abdullah <chwasiq0569>
2023-04-07 05:17:38 +0500

    Created Components

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,48 +1,21 @@
 import React from 'react';
 import styles from "./landingpage.module.scss"
-import Logo from "../../assets/icons/Logo.png"
-import CrossIcon from "../../assets/icons/crossicon.png"
 import ActionBtnIcon from '../../assets/icons/actiobtnico.png'
-import ArtWork from "../../assets/images/artwork.png"
-import DetailsIcon from "../../assets/icons/detailsicon.png"
 import HoverActionIcon from "../../assets/icons/hoveractionicon.png"
+import Navbar from '../../Components/Navbar/Navbar';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import PopUp from '../../Components/PopUp/PopUp';
 
 const LandingPage = () => {
 
     const [showPopUp, setShowPopUp] = React.useState(false);
 
     return (<div className={styles.landingPageContainer}>
-        <div className={styles.navbarContainer}>
-            <div className={styles.navbarLeftSide}>
-                <div className={styles.cornerIconsContainer}>
-                    <img src={Logo} alt="logo" />
-                </div>
-                <p>ENTRY TEST_</p>
-            </div>
-            <div className={styles.navbarRightSide}>
-                <p className={styles.dullText}>C:\EXERCISE\PROGRAMS\01.EXE</p>
-                <div className={styles.cornerIconsContainer}>
-                    <img src={CrossIcon} alt="logo" />
-                </div>
-            </div>
-        </div>
+        <Navbar />
         <div className={styles.contentContainer}>
-            <div className={styles.sidebarContainer}>
-                <div className={styles.verticalTextContainer}>
-                    <p>// THE MODERN COMPANY</p>
-                </div>
-                <div className={styles.codeContainer}>
-                    <p>001</p>
-                </div>
-            </div>
+            <Sidebar />
             <div className={showPopUp ? styles.mainContentBlur + " " + styles.mainContent : styles.mainContent}>
-                {showPopUp && <div className={styles.revealedArtWorkContainer}>
-                    <img src={ArtWork} alt='revealed_image' />
-                    <div className={styles.details}>
-                        <img src={DetailsIcon} alt="" />
-                        <p>DATA KEY</p>
-                    </div>
-                </div>}
+                {showPopUp && <PopUp />}
                 <button onClick={() => setShowPopUp(!showPopUp)} className={styles.actioBtn}>
                     <img className={styles.unhoveredImg} src={ActionBtnIcon} />
                     <img className={styles.hoveredImg} src={HoverActionIcon} />
